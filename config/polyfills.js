f1e15e18de356ac5e763b9b1742eb7e366012b6b
Antti Pihlaja <antti.pihlaja@mediasignal.fi>
2018-01-15 13:24:17 +0200

    Fix Object.assign conflict with polyfill.io

--- config/polyfills.js
+++ config/polyfills.js
@@ -21,7 +21,11 @@ require('whatwg-fetch');
 
 // Object.assign() is commonly used with React.
 // It will use the native implementation if it's present and isn't buggy.
-Object.assign = require('object-assign');
+var assign = require('object-assign');
+
+if (assign !== Object.assign) {
+  Object.assign = assign;
+}
 
 // In tests, polyfill requestAnimationFrame since jsdom doesn't provide it yet.
 // We don't polyfill it in the browser--this is user's responsibility.
